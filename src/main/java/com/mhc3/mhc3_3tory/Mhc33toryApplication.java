816e10961312b2b67aed804a32df9408c426736e
sungyeon <csy357@gmail.com>
2022-09-04 11:31:20 +0900

    add - test api, first commit

---
+++ src/main/java/com/mhc3/mhc3_3tory/Mhc33toryApplication.java
@@ -0,0 +1,13 @@
+package com.mhc3.mhc3_3tory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mhc33toryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mhc33toryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mhc3/mhc3_3tory/Mhc33toryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhc3.mhc3_3tory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mhc33toryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
